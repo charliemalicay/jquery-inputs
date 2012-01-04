0366fed151630e27f91be16871a956143eb7e474
D. Dotsenko <dotsa@hotmail.com>
2012-01-03 22:27:21 -0800

    Redressed plugin to be AMD-loader compatible. Tweaked readme.

--- README.markdown
+++ README.markdown
@@ -1,55 +1,54 @@
 # jquery-inputs plugin
 
-jquery-inputs is a jQuery plugin that allows set/get on form inputs using hierarchical JSON structures
+jquery-inputs is a jQuery plugin that allows to set/get form inputs using hierarchical JSON structures
 
-Cloned from [dshimkoski/jquery-inputs](http://github.com/dshimkoski/jquery-inputs/ "dshimkoski/jquery-inputs"). 
+This version is a (significant rewrite of and) deviation of [dshimkoski/jquery-inputs](http://github.com/dshimkoski/jquery-inputs/ "dshimkoski/jquery-inputs"). 
 
-The folowing additions are made on top of original code:
+The following backward-INcompatible changes were made on top of original code:
 *  .set() now also stores the default value as $(input element).data['defaultValue']
- (useful for checking for changes on a per-field basis and coloring the field on-change.)
+(useful for checking for changes on a per-field basis and coloring the field on-change.)
 *  support of underscore and square brackets as separators in input field names is removed. This shaves off a bit of time
-when parsing long forms.
-
-## Form Element Names
-
-Supports various naming conventions, e.g., demo_field_key, demo.field[key], and demo[field].key
-will all be treated as synonymous references to the same value.
-
-Empty brackets will automatically generate a 0-based index.
+when parsing long forms. Only dot (".") and underscore ("_") chars are now supported as key names separator.
+*  plugin now relies on some JavaScript 1.6+ methods of Object, Array objects. This means you must load a shim
+(like augment.js) for older browsers (IE less than 9). (Test for Array.isArray and Array.prototype.indexOf
+support as indicators of needing a shim.)
+*  Tunned for operation against jQuery 1.6+ where there is a tangible difference between .attr and .prop. Plugin
+is not tested against earlier versions, which are subjectively considered not to exist.
+*  Plugin is now AMD-loader compatible. You can now simply require(['jquery.inputs'], function($){ /* do things with $ */})
+We require 'jquery' internally. Please, insure that you hard-define 'jquery' before loading this plugin 
+by define('jquery', ...   or by setting up a 'path' for it as 'jqeury' in AMD loader's config.
 
 ## Usage
 
-Include jQuery and the plugin:
+Form elements may be named using a dot-delimited or underscore-delimited key name chain. 
+For example, "One.Two_Three" will be translated as reference to a value addressed as such in
+a JavaScript Object "obj['One']['Two']['Three'] = Value
 
-    <script src="jquery.inputs.js"></script>
+Add something like this to your page:
 
-Create a demo form:
 
+	<script src="jquery.js"></script>
+	<script src="jquery.inputs.js"></script>
 	<form id="demo-form">
-
 		<input type="text" name="demo_text" value="textval" />
-
-		<textarea name="demo_textarea">textareaval</textarea>
-
-		<select name="demo_select" multiple="multiple">
+		<textarea name="demo.textarea">textareaval</textarea>
+		<select name="demo.select" multiple="multiple">
 			<option value="option_a" selected="selected">Option A</option>
 			<option value="option_b" selected="selected">Option B</option>
 		</select>
-
 		<label><input type="radio" name="demo.radio" value="1" />1</label>
 		<label><input type="radio" name="demo.radio" value="2" />2</label>
-
-		<label><input type="checkbox" name="demo checkbox" value="1" checked="checked" />1</label>
-		<label><input type="checkbox" name="demo.checkbox" value="2" checked="checked" />2</label>
-
+		<label><input type="checkbox" name="demo.checkbox" value="1" />1</label>
+		<label><input type="checkbox" name="demo.checkbox" value="2" />2</label>
 	</form>
+
 
 Set/get values via javascript:
 
-    <script>
-      jQuery(document).ready(function($) {
-		
-        $('#demo-form').inputs('set', {
+
+	<script>
+	jQuery(document).ready(function($) {
+		$('#demo-form').inputs('set', {
 			demo: {
 				text: 'text',
 				textarea: 'text',
@@ -58,20 +57,15 @@ Set/get values via javascript:
 				select: ['option_a', 'option_b']
 			}
 		});
-		
 		console.log( $('#demo-form').inputs('get') );
-		
-      });
-    </script>
+	});
+	</script>
 
-See [QUnit tests](https://github.com/dshimkoski/jquery-inputs/blob/master/tests.html) for further examples.
 
-## Author
+See tests for more examples.
 
-[Denny Shimkoski](http://twitter.com/dennyshim)
-
-## Other
+## Authors
 
 [MIT License](http://www.opensource.org/licenses/mit-license.php)
 
-Copyright (c) 2011, Denny Shimkoski (denny.webdev -[at]- gmail -[dot]- com)
+See source header for full and most current Copyright attributions.
--- jquery.inputs.js
+++ jquery.inputs.js
@@ -1,144 +1,150 @@
-/*
- * jquery-inputs is a jQuery plugin that allows set/get on form inputs using hierarchical JSON structures
- *
- * Forked from: http://github.com/dshimkoski/jquery-inputs/
-   The following significant changes were applied:
-   - plethora of delimiters was dropped, simplifying name -> keys chain parsing into simple .split()
-     Only dot is now supported as a key separator:  keyA.keyB.keyC = value
-   - JavaScript version required was upped to 1.6 (to simplify array lookup and iterating logic). 
-     This means you pretty much must load some JS16 shims like 'augment.js' for old broswers to work with this plugin.
- *
- * MIT license: http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright (c) 2011, Denny Shimkoski (denny.webdev -[at]- gmail -[dot]- com )
- * Copyright (c) 2011, Daniel Dotsenko (ddotsenko -[at]- walnutcomputing -[dot]- com )
- */
- (function($, undefined){
+/* @license
+  jquery-inputs is a jQuery plugin that allows set/get on form inputs using hierarchical JSON structures
 
-    var getfn = function(jqselector) {
-        /* when provided, jqselector is a string that allows to select only input elements that are children of 
-        the stated selector. The selector is still evaluated in the context of "this" - the element on which you call .inputs('get')
-        Example calls with selector:
-        $(elem).inputs('get', '.changed')
-        This way only :input-matching elems that either have class 'changed' or are children of elems with class 'changed'
-        will be selected.
-        */
-        var $i
-        if (jqselector) {
-            $i = $(jqselector, this) // 'this' can be non-form. $.serialize* do not work on non-form or non-input obj. 
-            $i = $i.filter(':input').add($i.find(':input'))
-        } else {
-            $i = $(':input', this) // 'this' can be non-form. $.serialize* do not work on non-form or non-input obj. 
-        }
-
-        var scope = {}
-        $.each(
-            $i.serializeArray()
-            , function(){ 
-                applyValueToScope(this.name, this.value, scope) 
-            }
-        )
-        return scope
-    }
-
-    var applyValueToScope = function(name, value, scope) {
-        var keychain = name.replace('_','.').split('.')
-            ,lastkey = keychain.pop()
-            ,currentscope = scope
-            ,tmpscope = currentscope
-        keychain.forEach(function(key){
-            tmpscope = currentscope[key]
-            if (tmpscope == null) {
-                currentscope[key] = {}
-            }
-            else {
-                if (!$.isPlainObject(tmpscope)) {
-                    throw new TypeError("Value cannot be assigned to key '"+name+"' as another element on this path terminates with an non-object object.")
-                }
-            }
-            currentscope = currentscope[key]
-        })
-
-        tmpscope = currentscope[lastkey]
-        if (tmpscope == null) {
-            currentscope[lastkey] = value
-        }
-        else {
-            // ok, there is something already set for that key and competes for space with out value.
-            // this may be both ok (it's an array of values, or same exact value)ok, and not ok (any other outcome).
-            // we do NOT create arrays by guessing there should be one.
-            // our scoped value is still in tmpscope from 5+ lines above
-            if (value != tmpscope) {
-                if (Array.isArray(tmpscope) && typeof(value) == 'string') {
-                    // we determined earlier that this is going to be a ordered group of values. 
-                    // Likely a result of checkbox or radio elem group.
-                    if (tmpscope.indexOf(value) == -1) {
-                        tmpscope.push(value)
-                    }
-                } else if (typeof(value) == 'string' && typeof(tmpscope) == 'string') {
-                    currentscope[lastkey] = [tmpscope, value]
-                } else {
-                    throw new TypeError("Value cannot be assigned to key '"+name+"' as another element on this path continues with object chain.")
-                }
-            }
-        }
-    }
-    
-    var setfn = function(values) {
-        // jquery form (technically could be any element with nested inputs)
-        var $form = $(this)
-        // loop through form inputs
-        $form.find(':input').each(function(){
-            var $input = $(this)
-                , keys = $input.attr('name').replace('_','.').split('.')
-                , setflag = true
-                , scope = values
-            keys.forEach( function(key) {
-                try {
-                   scope = scope[key]
-                   if( scope == undefined ) {
-                       throw new TypeError("jQuery.Input: Path traversal in data object for '"+ key +"' of '" + $input.attr('name') + "' was cut short by an incompatible object")
-                   }
-                }
-                catch (ex) {
-                   setflag = false
-                }
-            })
-            
-            if ( $input.is(':checkbox, :radio') ) {
-                $input.prop('checked', false).data('defaultValue', false)
-                if ( setflag ) {
-                    if ( Array.isArray(scope) ) {
-                        if  ( scope.some( 
-                                function(item) {
-                                    return ( $input.prop('value') == new String(item) )
-                                })
-                            ) {
-                            $input.prop('checked', true).data('defaultValue', true)
-                        }
-                    } else if ( $input.prop('value') == new String(scope) ) {
-                        $input.prop('checked', true).data('defaultValue', true)
-                    }
-                }
-            } else if (setflag) {
-                $input.val(scope).data('defaultValue', scope)
-            } else {
-                $input.val('').data('defaultValue', '')
-            }
-        })
-    }
+  https://github.com/willowsystems/jquery-inputs
+  Originally a fork of http://github.com/dshimkoski/jquery-inputs/
  
-    var methods = {
-        set: setfn,
-        get: getfn
-    }
+  MIT license: http://www.opensource.org/licenses/mit-license.php
+ 
+  Copyright (c) 2011, Denny Shimkoski (denny.webdev -[at]- gmail -[dot]- com )
+  Copyright (c) 2012, Willow Systems Corporation (techs -[at]- willow-systems.com )
+ */
+(function(){
+	var window = this
+	, jquery_input_appender = function($){
 
-    $.fn.inputs = function(method) {
-        if ( methods[method] ) {
-            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ))
-        } else {
-            $.error( 'Method ' +  method + ' does not exist on jQuery.inputs' )
-        }
-    }
+		var getfn = function(jqselector) {
+			/* when provided, jqselector is a string that allows to select only input elements that are children of 
+			the stated selector. The selector is still evaluated in the context of "this" - the element on which you call .inputs('get')
+			Example calls with selector:
+			$(elem).inputs('get', '.changed')
+			This way only :input-matching elems that either have class 'changed' or are children of elems with class 'changed'
+			will be selected.
+			*/
+			var $i
+			if (jqselector) {
+				$i = $(jqselector, this) // 'this' can be non-form. $.serialize* do not work on non-form or non-input obj. 
+				$i = $i.filter(':input').add($i.find(':input'))
+			} else {
+				$i = $(':input', this) // 'this' can be non-form. $.serialize* do not work on non-form or non-input obj. 
+			}
+	
+			var scope = {}
+			$.each(
+				$i.serializeArray()
+				, function(){ 
+					applyValueToScope(this.name, this.value, scope) 
+				}
+			)
+			return scope
+		}
+		, applyValueToScope = function(name, value, scope) {
+			var keychain = name.replace('_','.').split('.')
+				,lastkey = keychain.pop()
+				,currentscope = scope
+				,tmpscope = currentscope
+			keychain.forEach(function(key){
+				tmpscope = currentscope[key]
+				if (tmpscope == null) {
+					currentscope[key] = {}
+				}
+				else {
+					if (!$.isPlainObject(tmpscope)) {
+						throw new TypeError("Value cannot be assigned to key '"+name+"' as another element on this path terminates with an non-object object.")
+					}
+				}
+				currentscope = currentscope[key]
+			})
+	
+			tmpscope = currentscope[lastkey]
+			if (tmpscope == null) {
+				currentscope[lastkey] = value
+			}
+			else {
+				// ok, there is something already set for that key and competes for space with out value.
+				// this may be both ok (it's an array of values, or same exact value)ok, and not ok (any other outcome).
+				// we do NOT create arrays by guessing there should be one.
+				// our scoped value is still in tmpscope from 5+ lines above
+				if (value != tmpscope) {
+					if (Array.isArray(tmpscope) && typeof(value) == 'string') {
+						// we determined earlier that this is going to be a ordered group of values. 
+						// Likely a result of checkbox or radio elem group.
+						if (tmpscope.indexOf(value) == -1) {
+							tmpscope.push(value)
+						}
+					} else if (typeof(value) == 'string' && typeof(tmpscope) == 'string') {
+						currentscope[lastkey] = [tmpscope, value]
+					} else {
+						throw new TypeError("Value cannot be assigned to key '"+name+"' as another element on this path continues with object chain.")
+					}
+				}
+			}
+		}
+		, setfn = function(values) {
+			// jquery form (technically could be any element with nested inputs)
+			var $form = $(this)
+			// loop through form inputs
+			$form.find(':input').each(function(){
+				var $input = $(this)
+					, keys = $input.attr('name').replace('_','.').split('.')
+					, setflag = true
+					, scope = values
+				keys.forEach( function(key) {
+					try {
+					   scope = scope[key]
+					   if( scope == undefined ) {
+						   throw new TypeError("jQuery.Input: Path traversal in data object for '"+ key +"' of '" + $input.attr('name') + "' was cut short by an incompatible object")
+					   }
+					}
+					catch (ex) {
+					   setflag = false
+					}
+				})
+				
+				if ( $input.is(':checkbox, :radio') ) {
+					$input.prop('checked', false).data('defaultValue', false)
+					if ( setflag ) {
+						if ( Array.isArray(scope) ) {
+							if  ( scope.some( 
+									function(item) {
+										return ( $input.prop('value') == new String(item) )
+									})
+								) {
+								$input.prop('checked', true).data('defaultValue', true)
+							}
+						} else if ( $input.prop('value') == new String(scope) ) {
+							$input.prop('checked', true).data('defaultValue', true)
+						}
+					}
+				} else if (setflag) {
+					$input.val(scope).data('defaultValue', scope)
+				} else {
+					$input.val('').data('defaultValue', '')
+				}
+			})
+		}
+		, methods = {
+			set: setfn,
+			get: getfn
+		}
 
-})(jQuery)
+		$.fn.inputs = function(method) {
+			if ( methods[method] ) {
+				return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ))
+			} else {
+				$.error( 'Method ' +  method + ' does not exist on jQuery.inputs' )
+			}
+		}
+
+		return $
+	}
+	
+	if ( typeof define === "function" && define.amd != null) {
+		// AMD-loader compatible resource declaration
+		define(['jquery'], function($){return jquery_input_appender($) /*returning jQuery back, with plugin attached.*/} )
+	} else {
+		// global-polluting outcome.
+		jquery_input_appender(window.jQuery)
+	}
+
+}).call(this)
